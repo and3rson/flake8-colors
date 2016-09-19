b35c099c391a72356e6fd521367afaccfba7f467
Andrew Dunai <andunai@gmail.com>
2016-09-19 17:54:07 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+import setuptools
+
+requires = [
+    "flake8 > 3.0.0",
+]
+
+setuptools.setup(
+    name="flake8-colors",
+    license="MIT",
+    version="0.1.0",
+    description="Error highlight plugin for Flake8.",
+    author="Andrew Dunai",
+    author_email="andrew@dun.ai",
+    url="https://gitlab.com/and3rson/flake8_colors",
+    packages=[
+        "flake8_colors",
+    ],
+    install_requires=requires,
+    entry_points={
+        'flake8.extension': [
+            'flake8-colors = flake8_colors:ColorFormatter',
+        ],
+    },
+    classifiers=[
+        "Framework :: Flake8",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+)
